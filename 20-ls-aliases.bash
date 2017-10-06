1d0dbd8164fbb25df5491a6e40c75c63ebd18d9c
James Service <james.service@riverbed.com>
2017-10-06 14:17:51 +0100

    Remove forced colour for ls.

--- 20-ls-aliases.bash
+++ 20-ls-aliases.bash
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# Detect which `ls` flavor is in use
-if ls --color > /dev/null 2>&1; then # GNU `ls`
-  colorflag="--color"
-else # OS X `ls`
-  colorflag="-G"
-fi
-
-# Always use color output for `ls`
-alias ls="command ls ${colorflag}"
-
 # List all files in long format
 alias ll='ls -lh'
 
