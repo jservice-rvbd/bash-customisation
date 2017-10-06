2eb21ac958a9bab1d07ddd1c8f26ff726503004b
James Service <james.service@riverbed.com>
2017-10-06 14:24:17 +0100

    Set GOROOT depending on which of the likely two paths exists.

--- 32-go-vars.bash
+++ 32-go-vars.bash
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 # Set up Go paths
+
+for goroot in /usr/local/go /usr/local/opt/go/libexec; do
+	if [ -d "$goroot" ]; then
+		export GOROOT="$goroot"
+		add-path "$GOROOT/bin"
+		break
+	fi
+done
+
 export GOPATH="$HOME/go"
-export GOROOT="/usr/local/opt/go/libexec"
 add-path "$GOPATH/bin" "$GOROOT/bin"
